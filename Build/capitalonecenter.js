b43246e6f5cffce9d0fc7a4929547155a77de4a2
mburlinson <mburlinson@ifmm.com>
2023-05-26 15:17:49 -0400

    Update capitalonecenter.js

--- Build/capitalonecenter.js
+++ Build/capitalonecenter.js
@@ -1,53 +1,45 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var buildUrl = "https://mburlinson.github.io/capitalonecenter18/Build";
-    var globalunityinstance = null;
-    var loaderUrl = buildUrl + "/COcore18.loader.js";
-    var config = {
-        dataUrl: buildUrl + "/COcore18.data.unityweb",
-        frameworkUrl: buildUrl + "/COcore18.framework.js.unityweb",
-        codeUrl: buildUrl + "/COcore18.wasm.unityweb",
-        streamingAssetsUrl: "StreamingAssets",
-        companyName: "DefaultCompany",
-        productName: "Core",
-        productVersion: "0.1",
-    };
+  var buildUrl = "https://mburlinson.github.io/capitalonecenter18/Build";
+  var globalunityinstance = null;
+  var loaderUrl = buildUrl + "/COcore18.loader.js";
+  var config = {
+    dataUrl: buildUrl + "/COcore18.data.unityweb",
+    frameworkUrl: buildUrl + "/COcore18.framework.js.unityweb",
+    codeUrl: buildUrl + "/COcore18.wasm.unityweb",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "DefaultCompany",
+    productName: "Core",
+    productVersion: "0.1",
+  };
 
-    var container = document.querySelector("#unity-container");
-    var canvas = document.querySelector("#unity-canvas");
-    var loadingBar = document.querySelector("#unity-loading-bar");
-    var progressBarFull = document.querySelector("#unity-progress-bar-full");
-    var fullscreenButton = document.querySelector("#unity-fullscreen-button");
-    var mobileWarning = document.querySelector("#unity-mobile-warning");
+  var container = document.querySelector("#unity-container");
+  var canvas = document.querySelector("#unity-canvas");
+  var loadingBar = document.querySelector("#unity-loading-bar");
+  var progressBarFull = document.querySelector("#unity-progress-bar-full");
+  var mobileWarning = document.querySelector("#unity-mobile-warning");
 
-    if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-        container.className = "unity-mobile";
-        config.devicePixelRatio = 1;
-        mobileWarning.style.display = "block";
-        setTimeout(() => {
-            mobileWarning.style.display = "none";
-        }, 5000);
-    } 
+  if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+    container.className = "unity-mobile";
+    config.devicePixelRatio = 1;
+    mobileWarning.style.display = "block";
+    setTimeout(() => {
+      mobileWarning.style.display = "none";
+    }, 5000);
+  } 
 
-    loadingBar.style.display = "block";
+  loadingBar.style.display = "block";
 
-    var script = document.createElement("script");
-    script.src = loaderUrl;
-    script.onload = () => {
-        createUnityInstance(canvas, config, (progress) => {
-            progressBarFull.style.width = 100 * progress + "%";
-        }).then((unityInstance) => {
-            loadingBar.style.display = "none";
-            globalunityinstance = unityInstance;
-            if (fullscreenButton !== null) {
-                fullscreenButton.onclick = () => {
-                    unityInstance.SetFullscreen(1);
-                };
-            } else {
-                console.error('Fullscreen button element not found');
-            }
-        }).catch((message) => {
-            alert(message);
-        });
-    };
-    document.body.appendChild(script);
+  var script = document.createElement("script");
+  script.src = loaderUrl;
+  script.onload = () => {
+    createUnityInstance(canvas, config, (progress) => {
+      progressBarFull.style.width = 100 * progress + "%";
+    }).then((unityInstance) => {
+      loadingBar.style.display = "none";
+      globalunityinstance = unityInstance;
+    }).catch((message) => {
+      alert(message);
+    });
+  };
+  document.body.appendChild(script);
 });
